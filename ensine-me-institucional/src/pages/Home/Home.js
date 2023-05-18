5b5f0508a09db0f7977028df228ff13ce578fa9e
pineapplentt <jvsalss@gmail.com>
2023-05-18 18:55:59 -0300

    commit pra trocar de pc

--- ensine-me-institucional/src/pages/Home/Home.js
+++ ensine-me-institucional/src/pages/Home/Home.js
@@ -1,38 +1,39 @@
 import styles from "generalStyle/styles.module.css"
-import styles from "../Home/style/como-funciona.module.css"
+// import sComoFunciona from "../Home/style/como-funciona.module.css"
+import sMenu from "generalStyle/menu.module.css"
 
 const Home = () => {
     return (
         <>
             <div className={styles.telaInicial}>
-                <nav className="menu">
-                    <div className="menu__logo-container">
-                        <img src="./img/logo/images/svg/icon-modified.svg" alt="" className="menu__logo"/>
+                <nav className={sMenu.menu}>
+                    <div className={sMenu.menuLogoContainer}>
+                        <img src="./img/logo/images/svg/icon-modified.svg" alt="" className={sMenu.menuLogo}/>
                         <h2>ensine.me</h2>
                     </div>
-                    <div className="menu__opcoes-container">
-                        <ul className="menu__opcoes">
-                            <li><a className="opcao-hamburguer">Home</a></li>
-                            <li><a className="opcao-hamburguer">Quem somos</a></li>
-                            <li><a className="opcao-hamburguer">O que oferecemos</a></li>
-                            <li><a className="opcao-hamburguer">Como funciona</a></li>
-                            <li><a className="opcao-hamburguer">Depoimentos</a></li>
+                    <div className={sMenu.menu}>
+                        <ul className={sMenu.menuOpcoesContainer}>
+                            <li><a className={sMenu.opcaoHamburguer}>Home</a></li>
+                            <li><a className={sMenu.opcaoHamburguer}>Quem somos</a></li>
+                            <li><a className={sMenu.opcaoHamburguer}>O que oferecemos</a></li>
+                            <li><a className={sMenu.opcaoHamburguer}>Como funciona</a></li>
+                            <li><a className={sMenu.opcaoHamburguer}>Depoimentos</a></li>
                         </ul>
                     </div>
-                    <div className="menu__hamburguer-container">
+                    <div className={sMenu.menuHamburguerContainer}>
                         <input type="checkbox" name="ipt-menu-hamburguer" id="ipt-menu-hamburguer"/>
-                        <label for="ipt-menu-hamburguer" className="hamburguer-container__label">
-                            <span className="menu__container-imagem-burguer"></span>
+                        <label for="ipt-menu-hamburguer" className={sMenu.hamburguerContainerLabel}>
+                            <span className={sMenu.menuContainerImagemBurguer}></span>
                         </label>
-                        <div className="menu__hamburguer-opcoes-container">
-                            <ul className="menu__hamburguer-opcoes">
-                                <li><a href="#" className="opcao-hamburguer">Home</a></li>
-                                <li><a href="#" className="opcao-hamburguer">Quem somos</a></li>
-                                <li><a href="#" className="opcao-hamburguer">O que oferecemos</a></li>
-                                <li><a href="#" className="opcao-hamburguer">Como funciona</a></li>
-                                <li><a href="#" className="opcao-hamburguer">Depoimentos</a></li>
+                        <div className={sMenu.menuContainerImagemBurguer}>
+                            <ul className={sMenu.menuHamburguerOpcoesContainer}>
+                                <li><a className={sMenu.opcaoHamburguer}>Home</a></li>
+                                <li><a className={sMenu.opcaoHamburguer}>Quem somos</a></li>
+                                <li><a className={sMenu.opcaoHamburguer}>O que oferecemos</a></li>
+                                <li><a className={sMenu.opcaoHamburguer}>Como funciona</a></li>
+                                <li><a className={sMenu.opcaoHamburguer}>Depoimentos</a></li>
                             </ul>
-                            <a className="btn-acessar" href="#">Acessar</a>
+                            <a className={sMenu.btnAcessar} href="#">Acessar</a>
                         </div>
                     </div>
                 </nav>
